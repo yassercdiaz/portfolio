68c711230830755be3bec84b3e335726a41851e2
yassercdiaz <yass18.98@gmail.com>
2026-02-13 11:11:26 -0500

    feat: split home into idividual section components

--- js/main.js
+++ js/main.js
@@ -1,32 +1,42 @@
 async function loadComponent(id, path) {
-    try {
-        const response = await fetch(path);
-        const html = await response.text();
-        const element = document.getElementById(id);
+  try {
+    const response = await fetch(path);
+    const html = await response.text();
+    const element = document.getElementById(id);
 
-        if (!element) {
-            console.warn(`Elemento con id "${id}" no encontrado.`);
-            return;
-        }
-
-        element.innerHTML = html;
-    } catch (error) {
-        console.error('Error cargando componente:', error);
+    if (!element) {
+      console.warn(`Elemento con id "${id}" no encontrado.`);
+      return;
     }
+
+    element.innerHTML = html;
+  } catch (error) {
+    console.error("Error cargando componente:", error);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadComponent('header', 'components/header.html');
-    loadComponent('footer', 'components/footer.html');
-    loadComponent('main-content', 'components/home.html');
-})
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header", "components/header.html");
+  loadComponent("footer", "components/footer.html");
+  loadComponent("main-content", "components/home.html");
 
-document.addEventListener('click', (e) => {
-    if (e.target.id === 'email-link') {
-        e.preventDefault();
-        navigator.clipboard.writeText('yass18.98@gmail.com')
-            .then(() => alert('Correo electrónico copiado al portapapeles'))
-            .catch(() => alert('No se pudo copiar el correo electrónico'))
-            .catch((error) => console.error('Error al copiar el correo electrónico:', error));
-    }    
+  loadComponent("about-section", "components/about.html");
+  loadComponent("education-section", "components/education.html");
+  loadComponent("projects-section", "components/projects.html");
+  loadComponent("certifications-section", "components/certifications.html");
+  loadComponent("skills-section", "components/skills.html");
+  loadComponent("contact-section", "components/contact.html");
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "email-link") {
+    e.preventDefault();
+    navigator.clipboard
+      .writeText("yass18.98@gmail.com")
+      .then(() => alert("Correo electrónico copiado al portapapeles"))
+      .catch(() => alert("No se pudo copiar el correo electrónico"))
+      .catch((error) =>
+        console.error("Error al copiar el correo electrónico:", error),
+      );
+  }
 });
